bc243d36849331be9d977c01c9f4cc58620005c3
Nick Singleton <nic.z@me.com>
2021-05-19 17:14:16 +0100

    Make functions more efficient; add async versions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,4 @@
 export function getVtimezone(timezone: string): string|null;
 export function getVtimezoneComponent(timezone: string): string|null;
+export function getVtimezoneAsync(timezone: string): Promise<string>;
+export function getVtimezoneComponentAsync(timezone: string): Promise<string>;
